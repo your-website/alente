192bf93436b2fe9300b1da563e118cd660368039
Roman Kolpachev <roman.kolpachev@yandex.ru>
2024-05-09 14:15:25 +0200

    test

--- test.ts
+++ test.ts
@@ -5,7 +5,8 @@ async function test() {
 async function test2() {
   return await test();
 }
-
+var bodyParser = "test";
+// var errorHandler = require('errorhandler');
 async function test3() {
   var bodyParser = "test";
   return await test2();
